1027ac3e22bd008932c7f13265e7d785e8a2e61c
Jun Sakata <j.sakata@dr-ubie.com>
2018-10-31 20:14:12 +0900

    add app2

---
+++ app2/src/main/java/hello/HelloController.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring Boot!";
+    }
+
+    @RequestMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+    
+}
